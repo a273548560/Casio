c88179299d2e321be6902351c03bf35c0e194b55
a273548560 <58450374+a273548560@users.noreply.github.com>
2020-01-07 09:36:33 +0800

    创建项目

---
+++ Casio/src/main/java/com/accp/CasioApplication.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasioApplication.class, args);
+	}
+
+}
